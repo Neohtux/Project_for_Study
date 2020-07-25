90ef82fc16bf2f480271f98d4c27dbc041e76bb0
junhyuckbok <bokjun@a.ut.ac.kr>
2020-07-25 21:05:06 +0900

    while(true)를 이용하여 Client 소켓을 Accept() 하는 부분에 무한정 대기하는 현상을 Client가 들어오는 연결시도를 수락하는 비동기작업을 AcceptAsync를 사용하여 대체하면서. Listener.cs 클래스를 새로 만듦.
    Listener(이하 리스너) 클래스에서 비동기 소켓 작업의
    Accept(SocketAsyncEventArgs e)사용할 파라미터를 생성하였음.
    
    이벤트 핸들러로 OnAcceptCompleted를 생성하여 클라이언트의 접속요청에
    Accept가 발생하면 이벤트 메서드 호출.
    
    추후 변경 사항 Receive와 Send를 Listner에서 따로 분리 예정.

---
+++ Server_proj/Listener.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_proj
+{
+    class Listener
+    {
+        public Listener() { }
+        string my_host = Dns.GetHostName();
+
+        //MAC주소와 IP주소, hostname을 받아올 수 있는 hostEntry 생성
+        IPHostEntry hostEntry;
+        //네트워크 끝점을 AddressList[1] IP주소와, 포트번호8887로 나타낸다.
+        IPEndPoint endpoint;
+        //Socket 생성 (TCP로 설정, TCP시 소켓타입은 스트림)
+        Socket listen_socket;
+        //소켓 바인드
+
+
+        public void Init(SocketAsyncEventArgs Async_arg)
+        {
+            hostEntry = Dns.GetHostEntry(my_host);
+            endpoint = new IPEndPoint(hostEntry.AddressList[1], 8887);
+            listen_socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listen_socket.Bind(endpoint);
+            listen_socket.Listen(10);
+            
+            //Accept();
+            Start_Accept(Async_arg);
+        }
+        public void Start_Accept(SocketAsyncEventArgs asyncEventArgs)
+        {
+     
+            if (asyncEventArgs == null)
+            {
+                asyncEventArgs = new SocketAsyncEventArgs();
+                asyncEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            }
+            else asyncEventArgs.AcceptSocket = null;
+            
+            bool pending = listen_socket.AcceptAsync(asyncEventArgs);
+
+            if(!pending)
+            {
+                OnAcceptCompleted(null, asyncEventArgs);
+            }
+        }
+        public void OnAcceptCompleted(object obj, SocketAsyncEventArgs args)
+        {
+            if (args.LastOperation == SocketAsyncOperation.Accept)
+            {
+                Socket client_socket = args.AcceptSocket;
+                ProcessReceive(args,client_socket);
+                ProcessSend(args,client_socket);
+            }
+            Start_Accept(args);
+
+        }
+       
+        private void ProcessReceive(SocketAsyncEventArgs args,Socket client_Socket)
+        {
+            byte[] recv_buff = new byte[1024];
+
+            int client_data_length = client_Socket.Receive(recv_buff);
+            string client_data = Encoding.UTF8.GetString(recv_buff, 0, client_data_length);
+            Console.WriteLine($"[From Client ] : {client_data}");
+        }
+
+        private void ProcessSend(SocketAsyncEventArgs args, Socket client_socket)
+        {
+            byte[] send_message = Encoding.UTF8.GetBytes("Welcome to MMORPG Server");
+            client_socket.Send(send_message);
+        }
+    }
+}
