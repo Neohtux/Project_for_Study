a0ae5d60edfad1f9e75fc0e63ad9b42d79cdeaaa
neohtux <neohtux@daum.net>
2020-07-28 10:02:10 +0900

    IP 주소, 포트번호, EndPoint는 리스너에서 하기보다는 Servercore 메인에서 시작하고, 리스너는 EndPoint를 받아서 소켓을 생성하고 클라이언트의 접속요청을 대기하는게 맞는거 같다.

--- Server_proj/Program.cs
+++ Server_proj/Program.cs
@@ -10,15 +10,20 @@ namespace Server_proj
 {
   
     class Program : Listener
-    {
-        static SocketAsyncEventArgs arg;
-
-        
+    { 
         static void Main(string[] args)
         {
             // Server PC hostname을 얻어온다.
+            string my_host = Dns.GetHostName();
+            //MAC주소와 IP주소, hostname을 받아올 수 있는 hostEntry 생성
+            IPHostEntry hostEntry;
+            //네트워크 끝점을 AddressList[1] IP주소와, 포트번호8887로 나타낸다.
+            IPEndPoint endpoint;
+            hostEntry = Dns.GetHostEntry(my_host);
+            endpoint = new IPEndPoint(hostEntry.AddressList[1], 8887);
+
             Listener listener = new Listener();
-            listener.Init(arg);
+            listener.Init(endpoint);
 
             while (true) { };
         }
