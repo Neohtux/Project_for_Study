021bf95ebce93b131ade3d5dda37227e67185843
neohtux <neohtux@daum.net>
2020-07-29 15:16:35 +0900

    Session에 Send 추가, Send 같은경우 매번 Async를 걸어놓기 보다는 임시 버퍼인 Queue에 byte[] 를 대기 시켜놓았다. 여러번 Send가 호출되면서 메시지를 보낼때보다 버퍼에 담아서 보내는것이 더 오버헤드가 적다고 생각하였다.
    Send 워커 스레드가 다음 작업을 할 수 있을때 (Is_send_wait ==false)일때
    queue에 쌓인 데이터를 Send 한다.
    
    SendAsync가 정상적으로 완료되어 체이닝된 OnSendComplted 가 호출되면 ,
    비동기 Send 스레드는 다시 Send를 실행할 수 있는 상태로
    대기 상태인 Is_send_wait의 값을 사용할 수 있게 false로 변경해준다.

--- Server_proj/Session.cs
+++ Server_proj/Session.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server_proj
@@ -10,19 +11,26 @@ namespace Server_proj
     class Session
     {
         Socket client_socket;
+        SocketAsyncEventArgs sendArgs = new SocketAsyncEventArgs();
+        Queue<byte[]> sendMessage_queue = new Queue<byte[]>();
+
+        bool Is_send_wait = false;
+        object _lock = new object();
 
         public void Start_Session(Socket socket)
         {
             client_socket = socket;
             SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
+            sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
 
-            //byte[] recv_buff = new byte[1024];
+            byte[] send_buff = new byte[1024];
+            send_buff = Encoding.UTF8.GetBytes("Welcome to Server !");
             recvArgs.SetBuffer(new byte[1024], 0,1024);
-
-            Start_Init(recvArgs);
+            Start_Receive(recvArgs);
+            Send(send_buff);
         }
-        void Start_Init(SocketAsyncEventArgs args)
+        void Start_Receive(SocketAsyncEventArgs args)
         {
             //리시브 비동기 호출
             bool pending = client_socket.ReceiveAsync(args);
@@ -33,22 +41,55 @@ namespace Server_proj
             } 
             
         }
+        void Send(byte[] Buffer)
+        {
+            lock (_lock)
+            {
+                sendArgs.SetBuffer(Buffer, 0, Buffer.Length);
+                sendMessage_queue.Enqueue(Buffer);
+
+                if (Is_send_wait == false)
+                    Start_Send(sendArgs);
+            }
+        }
+       
+        void Start_Send(SocketAsyncEventArgs args)
+        {
+            Is_send_wait = true;
+            byte[] buff = sendMessage_queue.Dequeue();
+            args.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = client_socket.SendAsync(args);
+
+            if (pending == false)
+            {
+                OnSendCompleted(null, args);
+            }
+        }
         public void Disconnect()
         {
             client_socket.Shutdown(SocketShutdown.Both);
             client_socket.Close();
         }
         #region Send/Receive
-
-        //멀티 스레드 작동 부분(danger zone!)
-        void OnRecvCompleted(object obj,SocketAsyncEventArgs args)
+        void OnSendCompleted(object obj, SocketAsyncEventArgs args)
         {
             if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
             {
+                Is_send_wait = false;
+            }
+         
+        }
+        //멀티 스레드 작동 부분(danger zone!)
+        void OnRecvCompleted(object obj,SocketAsyncEventArgs args)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
                 string client_data = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                 Console.WriteLine($"[From client] : {client_data}");
+                Start_Receive(args); //Receive 예약.
             }
-            Start_Init(args);
+            else Disconnect();
         }
         #endregion
     }
