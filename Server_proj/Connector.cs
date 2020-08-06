cb92be9db21b2053db4c8410994da73c098d6a39
neohtux <neohtux@daum.net>
2020-08-06 17:29:50 +0900

    2020-08-06 Connector 커밋 내용
        Server_Core에 새롭게 추가된 Connector.class 파일은
    
        Client의 컨텐츠에서
    
        Endpoint를 생성하여 Connector 객체의 인자로 넘겨준다.
    
        Connector 클래스 내부에는
        기존 Session을 상속받은 GameSession 클래스가 있고
        오버 라이딩 된 메서드를 이용하여 Server측과 데이터를 주고 받는다.
    
        기존 Server_Core의 모든 클래스들을 라이브러리화 시켰다.
        (Listener, Connector,Session)
        Listener는 서버의 컨텐츠에서 Listener 객체를 이용하여
        접솓된 클라이언트에게 데이터 전송을 위한 세션을 자동으로 생성되게
        하였다.
    
        Connector는 클라이언트 컨텐츠에서 Connector 객체를 이용하여
        서버측에 접속을 성공하면 Clinet의 세션을 자동으로 생성하여
        데이터 전송을 할수 있게 하였다.
    
        Session은 네트워크(연결,종료) 부분과 데이터 Send / Receive의 기능을
        담당하며 Session 자체는 클라이언트와 서버측에서 모두 사용됨으로써
        추상클래스로 하여 하위클래스 (여기선 GameSession)를 만들게하여
    
        제공되는 인터페이스 메서드를 이용하여 각 서버 - 클라이언트 간
        통신을 할수 있게 하였다.

---
+++ Server_proj/Connector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Core
+{
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes("Welcome to Server !");
+            Send(buffer);
+            Console.WriteLine($"OnConnected : {endPoint}");
+        }
+        public override void OnDisconnect(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnReceive(byte[] buffer, int ByteTransferred)
+        {
+            string client_data = Encoding.UTF8.GetString(buffer, 0, ByteTransferred);
+            Console.WriteLine($"[From client] : {client_data}");
+        }
+
+        public override void OnSend(byte[] buffer)
+        {
+            Console.WriteLine($"Transferred bytes : {buffer.Length}");
+        }
+    }
+    public class Connector
+    {
+        //클라이언트 to 서버 연결 시작.
+
+        public void Connect(IPEndPoint iPEndPoint)
+        {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.RemoteEndPoint = iPEndPoint;
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnectCompleted);
+
+            Socket socket = new Socket(args.RemoteEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            args.UserToken = socket;
+
+            Start_Connect(args);
+        }
+
+        //소켓 args 전달, 비동기 커넥트
+        private void Start_Connect(SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket;
+            bool pending = socket.ConnectAsync(args);
+
+            if (pending == false)
+                OnConnectCompleted(null, args);
+
+        }
+        //커넥트 이벤트 
+        private void OnConnectCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success)
+            {
+                GameSession session = new GameSession();
+                session.Start_Session(e.ConnectSocket);
+                session.OnConnected(e.RemoteEndPoint);
+            }
+        }
+
+    }
+}
