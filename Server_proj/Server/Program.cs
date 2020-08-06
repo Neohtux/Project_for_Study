c4b9e6f43d8c3a38ea712bf90c3a36bfe74d81d0
neohtux <neohtux@daum.net>
2020-08-06 17:48:46 +0900

    2020-08-06 Server 커밋 내용
    만들어진 Server_Core 클래스 라이브러리를 이용하여
    Listener 객체를 생성해서 클라이언트를 받는다.
    
    현재
    Clinet의 Connector와
    Server의 Listener는 모두 Session을 상속받은 GameSession을 사용중
    
    추후 Server와 Client에서는 GameSession을 새로 재정의 할 필요가 있음

--- Server_proj/Server/Program.cs
+++ Server_proj/Server/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
+using Server_Core;
 namespace Server
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+        }
+        public override void OnDisconnect(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnReceive(byte[] buffer, int ByteTransferred)
+        {
+            string client_data = Encoding.UTF8.GetString(buffer, 0, ByteTransferred);
+            Console.WriteLine($"[From client] : {client_data}");
+        }
+
+        public override void OnSend(byte[] buffer)
+        {
+            Console.WriteLine($"Transferred bytes : {buffer.Length}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // Server PC hostname을 얻어온다.
+            string my_host = Dns.GetHostName();
+            //MAC주소와 IP주소, hostname을 받아올 수 있는 hostEntry 생성
+            IPHostEntry hostEntry;
+            //네트워크 끝점을 AddressList[1] IP주소와, 포트번호8887로 나타낸다.
+            IPEndPoint endpoint;
+            hostEntry = Dns.GetHostEntry(my_host);
+            endpoint = new IPEndPoint(hostEntry.AddressList[1], 8887);
+
+            Listener listener = new Listener();
+            listener.Init(endpoint);
+
+            while (true) { };
         }
     }
 }
