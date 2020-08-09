e53182f92ecef1b485888c250848728ddfa20e01
junhyuckbok <bokjun@a.ut.ac.kr>
2020-08-09 16:54:30 +0900

    2020-08-09 클라이언트 업데이트 내용 프로젝트 최신 업데이트,
    시작 프로젝트
    Dummyclient , Server
    
    클래스 라이브러리 Server_Core
    
    -------------------클라이언트--------------------------
    Dummyclient/Program.cs
    Server_Core 라이브러리의 Session을 상속받은
    새로운 "클라이언트의 Gamesession" 을 생성,
    
    Connector 객체의 Connect 호출시
    connector.Connect(endpoint,새로운Gamesession)으로
    
    2번째 파라미터로 클라이언트에서 새로 생성한 객체를 전달하여
    클라이언트에서 새로 정의된 인터페이스 메소드를 이용
    -------------------------------------------------------

---
+++ DummyClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Server_Core;
+namespace DummyClient
+{
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes("Hi I'm Client !");
+            Send(buffer);
+            Console.WriteLine($"OnConnected : {endPoint}");
+        }
+        public override void OnDisconnect(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnReceive(byte[] buffer, int ByteTransferred)
+        {
+            string client_data = Encoding.UTF8.GetString(buffer, 0, ByteTransferred);
+            Console.WriteLine($"[From Server] : {client_data}");
+        }
+
+        public override void OnSend(byte[] buffer)
+        {
+            Console.WriteLine($"Transferred bytes : {buffer.Length}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // client PC hostname을 얻어온다.
+            string my_host = Dns.GetHostName();
+            //MAC주소와 IP주소, hostname을 받아올 수 있는 hostEntry 생성
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(my_host);
+            //네트워크 끝점을 AddressList[1] IP주소와, 포트번호8887로 나타낸다.
+            IPEndPoint endpoint = new IPEndPoint(hostEntry.AddressList[1], 8887);
+
+            while (true)
+            {
+                Connector connector = new Connector();
+                GameSession session = new GameSession();
+                connector.Connect(endpoint,session);
+
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+
+}
