bec6fa8d4edf417ade8e80209f0f4867953af990
neohtux <neohtux@daum.net>
2020-08-12 20:47:17 +0900

    2020-08-12 SendBuffer.cs 내용
    SendBuffer를 Session 클래스 외부에서 버퍼를 만들어준다음
    Send(외부에서 만들어준 버퍼)를 이용하여 수신측에 데이터를 보내준다.
    
    항상 고정된 크기의 패킷을 보내는것이 아니기때문 일정 크기의 버퍼를
    잡아놓고, 다음 send 때마다 새로 생성해주는 것이아닌 만들어 놓은
    버퍼를 잘라서 사용하고 크기가 모자라면 새로 만들어준다.
    
    Open 인터페이스를 이용하여 reserverSize로 버퍼를 잡아주고
    사용한 만큼 Close를 이용하여 해당 버퍼의 커서를 이동시켜준다.
    
    SendBuffer 내부의 byte[] _buffer를 읽는것은 다수의 스레드가
    동시에 읽을 순 있지만 쓰기 위한 작업은
    외부에서 OnConnected에서 SendBufferHelper를 이용하여
    한번씩 write 하게 된다. 상호배제를 위해 스레드가 독립적인
    자신의 자원을 갖기 위해 ThreadLocal을 사용.
    
    ThreadLocal 객체의 Value 프로퍼티를 사용하여
    SendBuffer 클래스 타입을 참조할 수 있다.

---
+++ Server_proj/SendBuffer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server_Core
+{
+    public class SendBufferHelper
+    {
+        public static ThreadLocal<SendBuffer> Current_Buffer = new ThreadLocal<SendBuffer>(() => { return null; });
+
+        public static int ChunkSize { get; set; } = 4096;
+
+        public static ArraySegment<byte> Open(int reserveSize)
+        {
+            if(Current_Buffer.Value == null)
+            {
+                Current_Buffer.Value = new SendBuffer(ChunkSize);
+            }
+
+            //남은공간(FreeSize)보다 만드려는 공간의 크기(reserveSize)가 큰경우
+            if (Current_Buffer.Value.FreeSize < reserveSize) 
+                Current_Buffer.Value = new SendBuffer(ChunkSize);
+
+            return Current_Buffer.Value.Open(reserveSize);
+        }
+
+        public static ArraySegment<byte> Close(int usedSize)
+        {
+            return Current_Buffer.Value.Close(usedSize);
+        }
+    }
+
+    public class SendBuffer
+    {
+        //[][][_u][][][]
+        byte[] _buffer;
+        int _usedSize = 0;
+        
+        //사용 가능한 버퍼의 크기 
+        public int FreeSize { get{ return _buffer.Length - _usedSize; } }
+
+        public SendBuffer(int chunkSize)
+        {
+            _buffer = new byte[chunkSize];
+        }
+        public static ArraySegment<byte> Empty { get; }
+
+        
+        public ArraySegment<byte> Open(int reserveSize)
+        {
+            //예약된 공간이, 남아있는 공간보다 작거나 같은경우
+            // _usedSize ~ reserveSize 크기의 byte ArrraySegment를 반환한다
+            if (reserveSize <= FreeSize)
+                return new ArraySegment<byte>(_buffer, _usedSize, reserveSize);
+
+            return Empty;
+        }
+
+        public ArraySegment<byte> Close(int usedSize)
+        {
+            ArraySegment<byte> segment = new ArraySegment<byte>(_buffer, _usedSize, usedSize);
+            _usedSize += usedSize;
+
+            return segment;
+        }
+    }
+}
