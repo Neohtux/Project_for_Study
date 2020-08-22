c656ee83432298c345d350d59a0bf13310b0f87f
neohtux <neohtux@daum.net>
2020-08-22 13:13:03 +0900

    2020-08-22 SessionManager 신규 클래스 내용
    [싱글톤 Instance 요약]
    SessionManager 클래스의 인스턴스를 최초 한번만 static 으로 할당하여
    그 메모리 인스턴스를 Server측과 Client측에서
    사용하기 위함.
    
    [세션 매니저 클래스기능]
    
    Client마다 Session을 만들어줄때, 각 Session에 세션별 ID를
    부여하여 session을 구분시켜주고 저장해주는 자료구조로 해쉬테이블
    (Dictionary)를 사용
    key값으로는 각 세션 ID, value에는 Dummyclient들의 session을
    가지고 있다.
    
    세션 매니저를 이용하여 각 세션 ID별로
    서버측으로 보내온 패킷을 브로드 캐스팅할떄 사용하려고 한다.

---
+++ Server_proj/SessionManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Core
+{
+    
+    public class SessionManager
+    {
+        #region Singleton
+        static SessionManager _session;
+  
+
+        public static SessionManager Instance
+        {
+            get
+            {
+                if (_session == null)
+                    _session = new SessionManager();
+                return _session;
+            }
+        }
+        #endregion
+        static Dictionary<int, Session> _sessions = new Dictionary<int, Session>();
+        private static int sessionid = 0;
+        object _lock = new object();
+
+        
+
+        public Session Generate(Session session)
+        {
+            lock(_lock)
+            {
+                int s_id = ++sessionid;
+                Session s = session;
+                s.SessionID = s_id;
+
+                _sessions.Add(s_id, s);
+                return s;
+            }
+        }
+
+       
+    }
+}
