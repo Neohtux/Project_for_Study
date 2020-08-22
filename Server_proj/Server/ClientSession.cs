e78afa55384eb4ea08a0a76a8a92da1ad5fec4f1
neohtux <neohtux@daum.net>
2020-08-22 13:43:20 +0900

    2020-08-22 ClientSession 신규 클래스 내용
    Session에서 새로 만든 PacketSession을 상속받는 클래스
    오버라이딩된 인터페이스를 이용하여
    Client의 메시지를 받거나 보내는 처리를 담당.
    
    OnRecvPacket의 buffer에는  Client가 보낸 패킷이 들어있다.
    
    [추가 예정사항]
    새로운 static Buffer를 만들어 브로드캐스팅을할 예정.
    PacketManager를 만들어서 Packet에 데이터를 쓰고 읽는 작업을 하고
    Packet의 데이터를 핸들링할 예정.

---
+++ Server_proj/Server/ClientSession.cs
@@ -0,0 +1,41 @@
+﻿using Server_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+
+        }
+        public override void OnDisconnect(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PlayerPacket packet = new PlayerPacket();
+            Console.WriteLine("되냐?");
+
+            packet.Des(buffer);
+
+            Console.WriteLine($"RecevPacket_Size : {packet.p_size},  packet_ID : {packet.p_id}");
+            Console.WriteLine($"Message : {packet.message}");
+            //
+           // Console.WriteLine($"Player ID : {packet.player_id}");
+            //Console.WriteLine($"Player Message : {packet.message}");
+        }
+
+        public override void OnSend(int length)
+        {
+            Console.WriteLine($"Transferred bytes : {length}");
+        }
+    }
+}
