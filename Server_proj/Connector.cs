3e7ff8f2d9d1e4f7b6ba8a67bc0ad37b8cf1b003
neohtux <neohtux@daum.net>
2020-08-24 15:29:54 +0900

    2020-08-24 Connector 클래스 수정 사항
    [기존 내용]
    Session _session 전역변수를 사용하여 클라이언트가 접속시
    session을 만들어주는 형태.
    
    문제점 : 클라이언트간 세션사용 중복으로 발생하는 충돌현상
    
    [변경 내용]
    Func<Session> 델리게이트를 사용하여 클라이언트가 접속할때마다
    각 클라이언트들을 위한 세션을 만들어줌으로써 세션 중복사용
    문제를 방어.

--- Server_proj/Connector.cs
+++ Server_proj/Connector.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server_Core
@@ -11,37 +12,50 @@ namespace Server_Core
   
     public class Connector
     {
-        Session _session;
-        public void Connect(IPEndPoint iPEndPoint, Session session)
+        Func<Session> _sessionFactory;
+        //Session _session;
+        public void Connect(IPEndPoint iPEndPoint, Func<Session> sessionFactory)
         {
-            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
-            args.RemoteEndPoint = iPEndPoint;
-            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnectCompleted);
-            _session = session;
-            Socket socket = new Socket(args.RemoteEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            args.UserToken = socket;
+            for (int i = 0; i < 10; ++i)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                _sessionFactory = sessionFactory;
+                args.RemoteEndPoint = iPEndPoint;
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnectCompleted);
 
-            Start_Connect(args);
+                Socket socket = new Socket(args.RemoteEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                args.UserToken = socket;
+
+                Start_Connect(args);
+            }
+
         }
 
         //소켓 args 전달, 비동기 커넥트
         private void Start_Connect(SocketAsyncEventArgs args)
         {
             Socket socket = args.UserToken as Socket;
+            if (socket == null) return;
             bool pending = socket.ConnectAsync(args);
 
             if (pending == false)
+            {
                 OnConnectCompleted(null, args);
+            }
+                
 
         }
         //커넥트 이벤트 
         private void OnConnectCompleted(object sender, SocketAsyncEventArgs e)
         {
+
             if (e.SocketError == SocketError.Success)
             {
+                Session _session = _sessionFactory.Invoke();
                 _session.Start_Session(e.ConnectSocket);
                 _session.OnConnected(e.RemoteEndPoint);
             }
+
         }
 
     }
