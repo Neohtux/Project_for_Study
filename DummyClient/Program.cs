67adacb714c7c5229da231d46c9db17c564d42aa
neohtux <neohtux@daum.net>
2020-08-24 15:38:35 +0900

    2020-08-24 메인 클래스 수정 사항
    [기존 내용]
    메인에서 세션 객체를 만들어
    Connector의 파라미터로 넘겨주는 형태.
    
    서버간 1:1 통신의 형태였으나 N:1 통신을 위해
    인터페이스를 변경.
    
    [변경 내용]
    새로운 세션 객체를 반환하는 람다메서드를 사용하여
    Func<Session> 델리게이트에 체이닝 될수 있도록 인터페이스 변경.

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -5,57 +5,31 @@ using System.Threading;
 using Server_Core;
 namespace DummyClient
 {
-    class GameSession : Session
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            ArraySegment<byte> buffer = SendBufferHelper.Open(4096);
-            string str = "Hi I'm Client";
-            byte[] buffer1 = Encoding.UTF8.GetBytes(str);
-            Array.Copy(buffer1, 0, buffer.Array, buffer.Offset, buffer1.Length);
-
-            ArraySegment<byte> send_buffer = SendBufferHelper.Close(buffer1.Length);
-            Send(send_buffer);
-            Console.WriteLine($"OnConnected : {endPoint}");
-        }
-        public override void OnDisconnect(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnDisconnected : {endPoint}");
-        }
-
-        public override int OnReceive(ArraySegment<byte> buffer, int ByteTransferred)
-        {
-            string client_data = Encoding.UTF8.GetString(buffer.Array, 0, ByteTransferred);
-            Console.WriteLine($"[From Server] : {client_data}");
-
-            return buffer.Count;
-        }
-
-        public override void OnSend(byte[] buffer)
-        {
-            Console.WriteLine($"Transferred bytes : {buffer.Length}");
-        }
-    }
+    
     class Program
     {
+
         static void Main(string[] args)
         {
             // client PC hostname을 얻어온다.
             string my_host = Dns.GetHostName();
             //MAC주소와 IP주소, hostname을 받아올 수 있는 hostEntry 생성
-
             IPHostEntry hostEntry = Dns.GetHostEntry(my_host);
             //네트워크 끝점을 AddressList[1] IP주소와, 포트번호8887로 나타낸다.
             IPEndPoint endpoint = new IPEndPoint(hostEntry.AddressList[1], 8887);
+            SessionManager Instance = new SessionManager();
 
-            while (true)
-            {
-                Connector connector = new Connector();
-                GameSession session = new GameSession();
-                connector.Connect(endpoint,session);
 
-                Thread.Sleep(100);
-            }
+            //connector.Connect(endpoint, () => { return new ServerSession(); });
+            Connector connector = new Connector();
+            
+            ServerSession session = new ServerSession();
+            connector.Connect(endpoint, ()=> { return new ServerSession(); });
+            
+            
+
+            while (true) {  }
+
 
         }
     }
