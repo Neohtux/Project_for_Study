a2c8719ad54ca014cad9feb8f7eec23f38908523
neohtux <neohtux@daum.net>
2020-08-27 14:38:15 +0900

    2020-08-27 SessionManager 신규 클래스 추가 사항
    SessionManager 싱글톤을 이용하여
    
    Session을 생성해주고 생성된 Session마다 sessionid를 부여
    sessionID를 통해 session을 구분시켜준다.
    
    해시테이블의 key값은 sessionid 이고 value 값은 id에 대응되는 세션이다

---
+++ Server_proj/Server/SessionManager.cs
@@ -0,0 +1,49 @@
+﻿using Server_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    
+    class SessionManager
+    {
+        #region Singleton
+        static SessionManager _session;
+  
+
+        public static SessionManager Instance
+        {
+            get
+            {
+                if (_session == null)
+                    _session = new SessionManager();
+                return _session;
+            }
+        }
+        #endregion
+        public static Dictionary<int, Session> _sessions = new Dictionary<int, Session>();
+        private static int sessionid = 0;
+        object _lock = new object();
+
+
+       
+        public Session Generate()
+        {
+            lock(_lock)
+            {
+                int s_id = ++sessionid;
+                ClientSession s = new ClientSession();
+                s.SessionID = s_id;
+
+                _sessions.Add(s_id, s);
+                return s;
+            }
+        }
+
+       
+    }
+}
