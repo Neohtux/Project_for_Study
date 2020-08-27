ad448508dcd59eb70da72a4048496deb0317fca7
neohtux <neohtux@daum.net>
2020-08-27 14:46:48 +0900

    2020-08-27 SessionManager 신규 추가 내용
    싱글톤 SessionManager을 사용하여
    세션을 만들고 해당 각 세션마다 id를 부여하여
    sessionid를 _sessions 라는 리스트에 저장한다.
    
    이 List의 담긴 session을 이용하여
    SendForEach()문을 통해 접속한 모든 세션의 데이터를 서버로 보낸다.

---
+++ DummyClient/SessionManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+     class SessionManager
+    {
+        #region Singleton
+        static SessionManager _session = new SessionManager();
+
+        public static SessionManager Instance
+        {
+            get
+            {
+                if (_session == null)
+                    _session = new SessionManager();
+                return _session;
+            }
+        }
+        #endregion
+        List<ServerSession> _sessions = new List<ServerSession>();
+        object _lock = new object();
+
+        public void SendForEach()
+        {
+            lock(_lock)
+            {
+                foreach(ServerSession session in _sessions)
+                {
+                    PlayerPacket packet = new PlayerPacket();
+                    packet.p_id = 1;
+                    packet.player_id = 121;
+                    packet.message = "Hi Server! I'm groot";
+                    packet.p_size += (ushort)(packet.message.Length * 2 + 22);
+                    ArraySegment<byte> segment = packet.Ser();
+
+                    session.Send(segment);
+                }
+            }
+        }
+
+        public ServerSession Generate()
+        {
+            lock (_lock)
+            {
+                ServerSession session = new ServerSession();
+                _sessions.Add(session);
+                return session;
+            }
+        }
+
+
+    }
+}
