2ed0e41af35da21c8ef133adc890de7d23d749e7
neohtux <neohtux@daum.net>
2020-08-27 14:49:24 +0900

    2020-08-27 ServerSession 변경사항
    Connect에서 패킷을 만들어 보낸 방식을
    SessionManager를 이용하여 패킷을 만들고,  보내는 테스트
    
    그외 특이사항 없음

---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,38 @@
+﻿using Server_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+        }
+
+
+        public override void OnDisconnect(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override int OnReceive(ArraySegment<byte> buffer, int ByteTransferred)
+        {
+            PlayerPacket packet = new PlayerPacket();
+            packet.Des(buffer);
+            Console.WriteLine($"[From Server] : {packet.message}");
+
+            return buffer.Count;
+        }
+
+        public override void OnSend(int length)
+        {
+            Console.WriteLine($"[Client]  전송 bytes : {length}");
+        }
+
+    }
+}
